d773d4c415abf3f6f2d250bae658a8c2a709ca66
antic11d <dimitrije.misa@gmail.com>
2019-05-14 13:50:43 +0200

    Getting local IP fixed

--- src/structs/Node/Node.go
+++ src/structs/Node/Node.go
@@ -73,15 +73,19 @@ func getMyIP() (string) {
 }
 
 func getLocalIP() string {
-	host, _ := os.Hostname()
-	addrs, _ := net.LookupIP(host)
-	for _, addr := range addrs {
-		if ipv4 := addr.To4(); ipv4 != nil {
-			fmt.Println("[getLocalIP]IPv4: ", ipv4)
-			return ipv4.String()
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				fmt.Println(separator+"Your local IP is:"+ipnet.IP.String()+separator)
+				return ipnet.IP.String()
+			}
 		}
 	}
-
 	return ""
 }
 
